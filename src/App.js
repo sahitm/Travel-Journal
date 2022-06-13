a35dfc49d7c4de5edd6322f04dbd6e583162bbb2
sahitm <sahit7varma@gmail.com>
2022-06-13 17:15:12 +0530

    --style added style for image and card description

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Navbar from "./components/Navbar"
 import Card from "./components/Card"
+import data from "./data"
 
 function App() {
+  const cards = data.map(item => {
+    return(
+      <Card 
+          {...item}
+      
+      />
+    )
+  })
   return (
     <div>
       <Navbar />
-      <Card />
+      {cards}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,34 @@
 import React from "react"
+import Licon from "../images/Licon.jpg"
 
-export default function Card(){
+export default function Card(props){
     return(
-        <div>
+        <div class="container">
             
+            <img className="card--img" src={props.imageUrl} alt="mountain"
+            ></img>
+
+
+            <section className="card--description">
+                
+                <section id="location">
+                <img src={Licon} alt="location icon"></img>
+                <p>{props.location}</p>
+                <a href={props.googleMapsUrl} target="_blank" >view on google maps</a>
+                </section>
+
+                
+
+                <h1 id="title">{props.title}</h1>
+
+                <section className="dates">
+                    <h5>{props.startDate}-</h5>
+                    <h5>{props.endDate}</h5>
+                </section>
+
+                <section className="img--desc">{props.description}</section>
+
+            </section>
         </div>
     )
 }
